02a6b65d74a69b1ee3a054502714e219f46ee423
kwonsunbin <sunbin1696@gmail.com>
2026-05-28 13:42:30 +0900

    [refactoring] page 4

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -2,6 +2,7 @@ package baseball;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
@@ -16,11 +17,15 @@ public class GameTest {
 
     @Test
     void 널_값을_입력하면_예외가_발생한다() {
-        assertThrows(IllegalArgumentException.class, () -> game.guess(null));
+        assertIllegalArgument(() -> game.guess(null));
     }
 
     @Test
     void 세_자리가_아닌_입력이면_예외가_발생한다() {
-        assertThrows(IllegalArgumentException.class, () -> game.guess("12"));
+        assertIllegalArgument(() -> game.guess("12"));
+    }
+
+    private void assertIllegalArgument(Executable executable) {
+        assertThrows(IllegalArgumentException.class, executable);
     }
 }
