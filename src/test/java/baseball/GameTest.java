db9a4fbd7c6ecc9246a0a3c3787ffb4ef1b20bb3
kwonsunbin <sunbin1696@gmail.com>
2026-05-28 13:41:46 +0900

    [refactoring] page 2

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -1,15 +1,21 @@
 package baseball;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class GameTest {
 
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+    }
+
     @Test
     void 널_값을_입력하면_예외가_발생한다() {
-        Game game = new Game();
-
         assertThrows(IllegalArgumentException.class, () -> game.guess(null));
     }
 }
